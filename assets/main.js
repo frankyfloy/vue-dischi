6eba97911989c00c3c5c3c2b227a77524ab2317a
Francesco Sica <fsica171@gmail.com>
2021-04-09 17:38:21 +0200

    ..

--- assets/main.js
+++ assets/main.js
@@ -2,25 +2,46 @@ Vue.config.devtools = true;
 
 var app = new Vue({
 
-
     el: '#root',
     data: {
         arrtracks: [],
-        generi : [],
-
+        genre : [],
+        arrtracksFiltered : [],
         selected: "",
-
     },
 
     mounted:function () {
-
-        axios.get('https://flynn.boolean.careers/exercises/api/array/music')
-        .then((response)  => {
-            if (response.data.success) {
-                this.arrtracks = response.data.response;
-
-            }
-        });
+        this.initArrtracks();
     },
 
+    methods: {
+        initArrtracks : function() {
+            axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+            .then((response)  => {
+                if (response.data.success) {
+                    this.arrtracks = response.data.response;
+                    this.arrtracks.filter(element =>{
+                        let tempGenre = element.genre;
+                        if (this.genre.indexOf(tempGenre) == -1 ) {
+                            this.genre.push(element.genre);
+                        }
+                    })
+                    this.arrtracksFiltered = this.arrtracks.slice();
+                }
+            })
+        },
+
+        findForGenre: function(){
+            // Update track on select All
+            if (this.selected === "") {
+                this.arrtracksFiltered = this.initArrtracks();
+            }
+
+            this.arrtracksFiltered = this.arrtracks.filter((element) => {
+                if (this.selected === element.genre) {
+                    return element;
+                }
+            })
+        },
+    },
 })
